55b8f8592fdd8398a4f64d742d92c1f1b4f21d0f
lleaff <lleaff@users.noreply.github.com>
2016-04-09 17:42:28 +0200

    block scope, destructuring in for..in

--- 01/duplicates.swift
+++ 01/duplicates.swift
@@ -1,9 +1,13 @@
-func duplicates<T: Hashable>(a: [T]) -> [(T, Int)] {
+func occurences<T: Hashable>(a: [T]) -> [T: Int] {
   var nums = [T: Int]()
   for x in a {
     nums[x] = nums[x] == nil ? 1 : nums[x]! + 1;
   }
-  return nums.filter() {
+  return nums
+}
+
+func duplicates<T: Hashable>(a: [T]) -> [(T, Int)] {
+  return occurences(a).filter() {
     let (_, occurences) = $0
     return occurences > 1
   }
@@ -11,4 +15,7 @@ func duplicates<T: Hashable>(a: [T]) -> [(T, Int)] {
 
 let testArray = ["one", "one", "one", "two", "two", "three"]
 
-print(duplicates(testArray))
+print(testArray)
+for (val, occurences) in duplicates(testArray) {
+  print("\(occurences) occurences of \(val)")
+}
